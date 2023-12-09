360546ed8ec41b786477d0455bcb7d339317eb6e
Paco van der Linden <pavadeli@gmail.com>
2023-12-09 20:58:23 +0100

    refactored day9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -6,16 +6,7 @@ fn part1(input: SS) -> isize {
 }
 
 fn eval_sequence1(input: SS) -> isize {
-    let mut numbers = input.split_whitespace().map(to_isize).collect_vec();
-    from_fn(|| {
-        let result = numbers
-            .iter()
-            .any(|v| *v != 0)
-            .then_some(*numbers.last().unwrap());
-        numbers = numbers.iter().tuple_windows().map(|(a, b)| b - a).collect();
-        result
-    })
-    .sum()
+    derivatives(input, |n| *n.last().unwrap()).sum()
 }
 
 fn part2(input: SS) -> isize {
@@ -23,20 +14,27 @@ fn part2(input: SS) -> isize {
 }
 
 fn eval_sequence2(input: SS) -> isize {
+    derivatives(input, |n| n[0])
+        .collect_vec()
+        .into_iter()
+        .rev()
+        .reduce(|a, b| b - a)
+        .unwrap()
+}
+
+fn derivatives(
+    input: SS,
+    mut select: impl FnMut(&[isize]) -> isize,
+) -> impl Iterator<Item = isize> {
     let mut numbers = input.split_whitespace().map(to_isize).collect_vec();
-    from_fn(|| {
-        let result = numbers
-            .iter()
-            .any(|v| *v != 0)
-            .then_some(*numbers.first().unwrap());
+    from_fn(move || {
+        if numbers.iter().all(|v| *v == 0) {
+            return None;
+        }
+        let result = select(&numbers);
         numbers = numbers.iter().tuple_windows().map(|(a, b)| b - a).collect();
-        result
+        Some(result)
     })
-    .collect_vec()
-    .into_iter()
-    .rev()
-    .reduce(|a, b| b - a)
-    .unwrap()
 }
 
 boilerplate! {
